ac6b67020defd762348bb8d0701176c095959e32
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-01-24 10:10:01 +0300

    Added route offices

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { OfficesComponent } from './pages/offices/offices.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'offices', component: OfficesComponent}
 ];
