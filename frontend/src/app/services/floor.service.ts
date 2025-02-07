7261d46e892820729e8c6fbb266d82728da2e6e3
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-07 10:49:16 +0300

    Added model and service for floor

---
+++ frontend/src/app/services/floor.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Floor } from './models/Floor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+  private apiUrl = "http://localhost:8080/api/floors";
+
+  constructor(private http: HttpClient) { }
+
+  // Получение этажей по ID офиса
+  getFloorsByOfficeId(id: number): Observable<Floor[]> {
+    const url = `${this.apiUrl}/office/${id}`;
+    return this.http.get<Floor[]>(url)
+      .pipe(
+        catchError(this.handleError<Floor[]>('getFloorsByOfficeId', []))
+      );
+  }
+
+  // Обработка ошибок
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      throw error.message;
+    };
+  }
+}
---
+++ frontend/src/app/services/models/Floor.ts
@@ -0,0 +1,10 @@
+export interface Floor {
+    IdFloor: number;
+    NumberFloor: number;
+    TotalWorkspace: number;
+    Scheme?: string;
+    IdOffice: number;
+    Square?: number;
+    // IdOfficeNavigation: Office;
+    // Rooms: Room[];
+}
