b90337c5073d816a10b7b72b40314971f60e8554
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-07 11:12:28 +0300

    Added search service

---
+++ frontend/src/app/services/search.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private apiUrl = "http://localhost:8080/api/search";
+
+  constructor(private http: HttpClient) { }
+
+  // Поиск по запросу
+  searchOffices(query: string): Observable<any> {
+    const params = new HttpParams().set('query', query);
+
+    return this.http.get<any>(this.apiUrl, { params })
+      .pipe(
+        catchError(this.handleError<any>('searchOffices'))
+      );
+  }
+
+  // Обработка ошибок
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      throw error.message;
+    };
+  }
+}
