919bacb34cff62c577b6a7e03891e415f9e16427
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-11 15:51:18 +0300

    Added takeUntilDestroyed

--- frontend/src/app/pages/offices/offices.component.ts
+++ frontend/src/app/pages/offices/offices.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import CardComponent from '../../components/card/card.component';
 import { OfficeService } from '../../services/controllers/office.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'offices',
@@ -14,10 +15,11 @@ export class OfficesComponent implements OnInit {
   constructor (private officeService: OfficeService) {}
 
   ngOnInit(): void {
-    this.officeService.getOffices().subscribe(
+    this.officeService.getOffices()
+    .pipe(takeUntilDestroyed())
+    .subscribe(
       response => {
         this.data = response;
-        console.log(response);
       },
       error => {
         console.error(error);
