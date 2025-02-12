c1434fd13ab27e1219594888514b12d16b6ef55b
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-12 11:35:41 +0300

    Fixed routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { OfficesComponent } from './pages/offices/offices.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { OfficeComponent } from './pages/offices/office/office.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', component: OfficesComponent},
     {path: 'login', component: LoginComponent},
     {path: 'offices', component: OfficesComponent},
     {path: 'registration', component: RegistrationComponent},
