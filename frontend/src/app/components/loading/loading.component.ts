e90ef2a5546d40bb9830d4c38997b1e01a23e4bf
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-13 15:44:25 +0300

    Modify loading screen

--- frontend/src/app/components/loading/loading.component.ts
+++ frontend/src/app/components/loading/loading.component.ts
@@ -1,27 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {
-    TuiDataList,
-    TuiDropdown,
-    TuiTextfield,
-} from '@taiga-ui/core';
-import {
-    TuiTabs,
-} from '@taiga-ui/kit';
-import { TuiNavigation } from '@taiga-ui/layout';
 
 @Component({
     selector: 'app-loading',
     standalone: true,
     exportAs: "Example1",
-    imports: [
-        FormsModule,
-        TuiDataList,
-        TuiDropdown,
-        TuiNavigation,
-        TuiTabs,
-        TuiTextfield,
-    ],
     templateUrl: './loading.component.html',
     styleUrl: './loading.scss',
 })
