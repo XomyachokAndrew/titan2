f3c2110597176cdaa64b100f29316ef34ae52852
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-21 09:47:53 +0300

    Fixed worker card

--- frontend/src/app/components/workerCard/worker.component.ts
+++ frontend/src/app/components/workerCard/worker.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { TuiIcon, TuiIconPipe } from '@taiga-ui/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TuiIconPipe } from '@taiga-ui/core';
 import { ICurrentWorkspace } from '../../services/models/CurrentWorkspace';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'worker',
   templateUrl: './worker.component.html',
-  imports: [TuiIcon, TuiIconPipe],
+  imports: [TuiIconPipe, CommonModule],
   styleUrls: ['./worker.scss'],
 })
 export class WorkerComponent {
   @Input() currentWorkspace!: ICurrentWorkspace;
+  @Input() isEdit: boolean = false;
+  @Output() workerClicked = new EventEmitter<ICurrentWorkspace>();
+
   constructor() { }
+
+  onClick() {
+    this.workerClicked.emit(this.currentWorkspace);
+  }
 }
