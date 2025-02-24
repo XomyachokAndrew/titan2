eb7794f121919053c26e340e8402d185f0890f6a
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-24 12:26:22 +0300

    Added methods for loads Departmens and Posts

--- frontend/src/app/pages/createWorker/createWorker.component.ts
+++ frontend/src/app/pages/createWorker/createWorker.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TuiInputModule } from '@taiga-ui/legacy';
-import { TuiButton } from '@taiga-ui/core';
+import { TuiInputModule, TuiSelectModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
+import { IDepartment } from '../../services/models/Department';
+import { TuiTextfield } from '@taiga-ui/core';
+import { TuiDataListWrapper } from '@taiga-ui/kit';
+import { DepartmentService } from '../../services/controllers/department.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
+import { PostService } from '../../services/controllers/post.service';
+import { IPost } from '../../services/models/Post';
 
 interface Employee {
   firstName: string;
@@ -16,12 +28,29 @@ interface Employee {
   selector: 'createWorker',
   templateUrl: './createWorker.component.html',
   styleUrls: ['./createWorker.scss'],
-  imports: [TuiButton, ReactiveFormsModule, TuiInputModule],
+  imports: [
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiTextfield,
+    TuiDataListWrapper,
+    TuiTextfieldControllerModule,
+    TuiSelectModule,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf,
+    CdkVirtualScrollViewport,
+  ],
 })
 export class CreateWorkerComponent {
   employeeForm: FormGroup;
+  protected departments!: IDepartment[];
+  protected posts!: IPost[];
+  private destroyRef = inject(DestroyRef);
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private departmentService: DepartmentService,
+    private postService: PostService,
+  ) {
     this.employeeForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -29,6 +58,44 @@ export class CreateWorkerComponent {
       position: ['', Validators.required],
       organization: ['', Validators.required],
     });
+
+    this.loadDepartment();
+  }
+
+  async loadDepartment() {
+    this.departmentService.getDepartments()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        catchError(error => {
+          console.error('Ошибка при обработке данных об отделах: ', error);
+          return of(null);
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          if (data) {
+            this.departments = data;
+          }
+        }
+      });
+  }
+
+  async loadPost() {
+    this.postService.getPosts()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        catchError(error => {
+          console.error('Ошибка при обработке данных о должностях: ', error);
+          return of(null);
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          if (data) {
+            this.posts = data;
+          }
+        }
+      });
   }
 
   onSubmit() {
