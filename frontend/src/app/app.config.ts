f1041ce4b23f7ee1970376b4fbc2da611f14b138
XomyachokAndrew <124427665+XomyachokAndrew@users.noreply.github.com>
2025-02-28 11:52:48 +0300

    Added russian localization

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withFetch } from "@angular/common/http";
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE } from '@taiga-ui/i18n';
+import { of } from "rxjs";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    NG_EVENT_PLUGINS
+    NG_EVENT_PLUGINS,
+    {
+      provide: TUI_LANGUAGE,
+      useValue: of(TUI_RUSSIAN_LANGUAGE)
+    }
   ]
 };
